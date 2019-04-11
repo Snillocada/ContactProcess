2c5528b91121ac7ec2cea70d1fac669ee000b33b
Snillocada <49535821+Snillocada@users.noreply.github.com>
2019-04-11 22:47:07 +0100

    Add files via upload
    Original Files

---
+++ ContactProccessLinux.mk
@@ -0,0 +1,120 @@
+##
+## Auto Generated makefile by CodeLite IDE
+## any manual changes will be erased      
+##
+## Debug
+ProjectName            :=ContactProccessLinux
+ConfigurationName      :=Debug
+WorkspacePath          :=C:/Users/Adam/Documents/M4R/ContactProcess
+ProjectPath            :=C:/Users/Adam/Documents/M4R/ContactProcess/ContactProccessLinux
+IntermediateDirectory  :=./Debug
+OutDir                 := $(IntermediateDirectory)
+CurrentFileName        :=
+CurrentFilePath        :=
+CurrentFileFullPath    :=
+User                   :=Adam
+Date                   :=11/04/2019
+CodeLitePath           :="C:/Program Files/CodeLite"
+LinkerName             :=C:/MinGW/bin/g++.exe
+SharedObjectLinkerName :=C:/MinGW/bin/g++.exe -shared -fPIC
+ObjectSuffix           :=.o
+DependSuffix           :=.o.d
+PreprocessSuffix       :=.i
+DebugSwitch            :=-g 
+IncludeSwitch          :=-I
+LibrarySwitch          :=-l
+OutputSwitch           :=-o 
+LibraryPathSwitch      :=-L
+PreprocessorSwitch     :=-D
+SourceSwitch           :=-c 
+OutputFile             :=$(IntermediateDirectory)/$(ProjectName)
+Preprocessors          :=
+ObjectSwitch           :=-o 
+ArchiveOutputSwitch    := 
+PreprocessOnlySwitch   :=-E
+ObjectsFileList        :="ContactProccessLinux.txt"
+PCHCompileFlags        :=
+MakeDirCommand         :=makedir
+RcCmpOptions           := 
+RcCompilerName         :=C:/MinGW/bin/windres.exe
+LinkOptions            :=  
+IncludePath            :=  $(IncludeSwitch). $(IncludeSwitch). 
+IncludePCH             := 
+RcIncludePath          := 
+Libs                   := 
+ArLibs                 :=  
+LibPath                := $(LibraryPathSwitch). 
+
+##
+## Common variables
+## AR, CXX, CC, AS, CXXFLAGS and CFLAGS can be overriden using an environment variables
+##
+AR       := C:/MinGW/bin/ar.exe rcu
+CXX      := C:/MinGW/bin/g++.exe
+CC       := C:/MinGW/bin/gcc.exe
+CXXFLAGS := -std=c++14 -Wall -g -O0 -Wall $(Preprocessors)
+CFLAGS   :=  -g -O0 -Wall $(Preprocessors)
+ASFLAGS  := 
+AS       := C:/MinGW/bin/as.exe
+
+
+##
+## User defined environment variables
+##
+CodeLiteDir:=C:\Program Files\CodeLite
+Objects0=$(IntermediateDirectory)/main.cpp$(ObjectSuffix) $(IntermediateDirectory)/particle.cpp$(ObjectSuffix) 
+
+
+
+Objects=$(Objects0) 
+
+##
+## Main Build Targets 
+##
+.PHONY: all clean PreBuild PrePreBuild PostBuild MakeIntermediateDirs
+all: $(OutputFile)
+
+$(OutputFile): $(IntermediateDirectory)/.d $(Objects) 
+	@$(MakeDirCommand) $(@D)
+	@echo "" > $(IntermediateDirectory)/.d
+	@echo $(Objects0)  > $(ObjectsFileList)
+	$(LinkerName) $(OutputSwitch)$(OutputFile) @$(ObjectsFileList) $(LibPath) $(Libs) $(LinkOptions)
+
+MakeIntermediateDirs:
+	@$(MakeDirCommand) "./Debug"
+
+
+$(IntermediateDirectory)/.d:
+	@$(MakeDirCommand) "./Debug"
+
+PreBuild:
+
+
+##
+## Objects
+##
+$(IntermediateDirectory)/main.cpp$(ObjectSuffix): main.cpp $(IntermediateDirectory)/main.cpp$(DependSuffix)
+	$(CXX) $(IncludePCH) $(SourceSwitch) "C:/Users/Adam/Documents/M4R/ContactProcess/ContactProccessLinux/main.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/main.cpp$(ObjectSuffix) $(IncludePath)
+$(IntermediateDirectory)/main.cpp$(DependSuffix): main.cpp
+	@$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/main.cpp$(ObjectSuffix) -MF$(IntermediateDirectory)/main.cpp$(DependSuffix) -MM main.cpp
+
+$(IntermediateDirectory)/main.cpp$(PreprocessSuffix): main.cpp
+	$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/main.cpp$(PreprocessSuffix) main.cpp
+
+$(IntermediateDirectory)/particle.cpp$(ObjectSuffix): particle.cpp $(IntermediateDirectory)/particle.cpp$(DependSuffix)
+	$(CXX) $(IncludePCH) $(SourceSwitch) "C:/Users/Adam/Documents/M4R/ContactProcess/ContactProccessLinux/particle.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/particle.cpp$(ObjectSuffix) $(IncludePath)
+$(IntermediateDirectory)/particle.cpp$(DependSuffix): particle.cpp
+	@$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/particle.cpp$(ObjectSuffix) -MF$(IntermediateDirectory)/particle.cpp$(DependSuffix) -MM particle.cpp
+
+$(IntermediateDirectory)/particle.cpp$(PreprocessSuffix): particle.cpp
+	$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/particle.cpp$(PreprocessSuffix) particle.cpp
+
+
+-include $(IntermediateDirectory)/*$(DependSuffix)
+##
+## Clean
+##
+clean:
+	$(RM) -r ./Debug/
+
+
---
+++ particle.h
@@ -0,0 +1,51 @@
+#ifndef _PARTICLE_H_
+#define _PARTICLE_H_
+#include <vector>
+#include <memory>
+#include <tuple>
+#include <set>
+#include <random>
+class particle
+{
+    static std::vector <std::vector<int>> stensil;
+    static std::set <std::vector<int>> particle_locations;
+    static int number_of_particles;
+    static double lambda;
+    static const double epsilon;
+    static double spawning_range;
+    static int dimension;
+    
+public:
+    std::vector<int> location;
+private:
+    std::set <std::vector <int>> spawning_sites;
+    
+
+
+public:
+
+    void initialize_system_locations(std::set <std::vector<int>>);
+    
+    int get_spawning_range() const;
+    
+    int get_num_sites() const;
+
+    std::unique_ptr<particle>  spawn_particle(int &offset);
+    
+    std::pair<std::unique_ptr<particle>,bool> poke_particle(int &rand, double prob);
+    static int get_num_particles();
+    static void reset_num_particles();
+    
+    std::set <std::vector<int>> get_particle_locations() const;
+    std::set <std::vector<int>> get_spawn_sites() const;
+    
+    void set_lambda(double);
+    void set_R(size_t new_val);
+    void set_dim(size_t new_val);
+    
+    particle(std::vector<int> loc =  std::vector<int>(dimension,0));
+    ~particle();
+
+};
+
+#endif // _PARTICLE_H_
